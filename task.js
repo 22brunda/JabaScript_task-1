6a7df0b878542d86b6f6da1b6efa2103f4170869
22brunda <bsrinivas@qwinix.io>
2018-03-15 09:51:44 +0530

    Completed

--- task.js
+++ task.js
@@ -1,11 +1,26 @@
-function msg(){
-	alert("Enter the empty Fields");
+document.getElementById("formhide").style.display="none";
+function showform(){
+	var Email=document.getElementById("Email").value
+	var Firstname = document.getElementById("Firstname").value;
+	var Lastname = document.getElementById("Lastname").value;
+	var Address=document.getElementById("Address").value
+	 if( Email == "" || Firstname == "" || Lastname == "" || Address == "" ) {
+	 	alert("Enter the empty Fields");
+	 } else {
+	 	document.getElementById("mail").innerHTML=Email;
+		document.getElementById("fn").innerHTML=Firstname;
+		document.getElementById("ln").innerHTML=Lastname;
+		document.getElementById("addr").innerHTML=Address;
+		document.getElementById("formhide").style.display="block";
+		document.getElementById("showform").style.display="none";
+	 }
+	
 }
 
 function shopOnFlipkart() {
    var x = document.getElementById("shop");
     if (x.style.display === "none") {
-        x.style.display = "block";
+        x.style.diksplay = "block";
     } else {
         x.style.display = "none";
     }
